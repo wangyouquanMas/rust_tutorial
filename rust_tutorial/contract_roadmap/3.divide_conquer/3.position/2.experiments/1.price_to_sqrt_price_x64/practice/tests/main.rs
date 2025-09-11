56a9244ce845f739eb3edd5cd11fec5f06bfc2ee
Your Name <you@example.com>
2025-09-11 16:05:45 +0800

    normalization

--- rust_tutorial/contract_roadmap/3.divide_conquer/3.position/2.experiments/1.price_to_sqrt_price_x64/practice/tests/main.rs
+++ rust_tutorial/contract_roadmap/3.divide_conquer/3.position/2.experiments/1.price_to_sqrt_price_x64/practice/tests/main.rs
@@ -18,7 +18,7 @@ fn test_price_to_sqrt_price_x64() {
 
     // state.sqrt_price_x64:583330181263634387 0.8466825204893473
     // step.sqrt_price_next_x64:521037230575395660 0.7978074153597088
-    let sqrt_price_x64 = 521339167259952073;  // 0.7978074153597088
+    let sqrt_price_x64 = 22592555198148960256;  // 0.7978074153597088
     let price_from_x64 = sqrt_price_x64_to_price(sqrt_price_x64, decimals_0, decimals_1);
     println!("price_from_x64: {}", price_from_x64);
 
