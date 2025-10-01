644579c1e9146811b05eefbc2d131f7786f9bdfb
Your Name <you@example.com>
2025-10-01 22:35:46 +0800

    personal position data structure

--- contract_roadmap/6.implementation/1.anchor_tutorial/fun-uniswap-v3/programs/fun-uniswap-v3/src/states/mod.rs
+++ contract_roadmap/6.implementation/1.anchor_tutorial/fun-uniswap-v3/programs/fun-uniswap-v3/src/states/mod.rs
@@ -2,3 +2,5 @@ pub mod config;
 pub use config::*;
 pub mod pool;
 pub use pool::*;
+pub mod personal_position;
+pub use personal_position::*;
---
+++ contract_roadmap/6.implementation/1.anchor_tutorial/fun-uniswap-v3/programs/fun-uniswap-v3/src/states/personal_position.rs
@@ -0,0 +1,41 @@
+
+#[account]
+#[derive(Default, Debug)]
+pub struct PersonalPositionState {
+    /// Bump to identify PDA
+    pub bump: [u8; 1],
+
+    /// Mint address of the tokenized position
+    pub nft_mint: Pubkey,
+
+    /// The ID of the pool with which this token is connected
+    pub pool_id: Pubkey,
+
+    /// The lower bound tick of the position
+    pub tick_lower_index: i32,
+
+    /// The upper bound tick of the position
+    pub tick_upper_index: i32,
+
+    /// The amount of liquidity owned by this position
+    pub liquidity: u128,
+
+    /// The token_0 fee growth of the aggregate position as of the last action on the individual position
+    pub fee_growth_inside_0_last_x64: u128,
+
+    /// The token_1 fee growth of the aggregate position as of the last action on the individual position
+    pub fee_growth_inside_1_last_x64: u128,
+
+    /// The fees owed to the position owner in token_0, as of the last computation
+    pub token_fees_owed_0: u64,
+
+    /// The fees owed to the position owner in token_1, as of the last computation
+    pub token_fees_owed_1: u64,
+
+    // Position reward info
+    pub reward_infos: [PositionRewardInfo; REWARD_NUM],
+    // account update recent epoch
+    pub recent_epoch: u64,
+    // Unused bytes for future upgrades.
+    pub padding: [u64; 7],
+}
